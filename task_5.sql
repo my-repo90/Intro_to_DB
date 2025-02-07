a55d4289b7bd15514410c03aaf809dba0f808b2d
my-repo90 <malakdev024@gmail.com>
2025-02-07 12:46:16 +0100

    2,d commit

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,4 @@
 #	TASK : Write a script that inserts a single row in the table customer (database alx_book_store) in your MySQL server.
-INSERT INTO Customers (customer_id , customer_name , email , address)
+INSERT INTO customer (customer_id , customer_name , email , address)
 VALUES
 (1 , "Cole Baidoo" , "cbaidoo@sandtech.com" , "123 Happiness Ave.");
--- task_6.sql
+++ task_6.sql
@@ -1,5 +1,5 @@
 #Write a script that inserts multiple rows in the table customer (database alx_book_store) in your MySQL server.
-INSERT INTO Customers (customer_id , customer_name , email , address)
+INSERT INTO customer (customer_id , customer_name , email , address)
 VALUES
 (2 , "Blessing Malik" , "bmalik@sandtech.com" , "124 Happiness  Ave."),
 (3 , "Obed Ehoneah" , "eobed@sandtech.com" , "125 Happiness  Ave."),
